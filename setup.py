2e54fae3fcf79bf6ed81b8aa17a84fe19a1fa794
Luke Canada <canadaluke888@gmail.com>
2025-06-13 19:55:54 -0400

    chore: add Python extension module, import test, and CI workflow

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, Extension
+
+module = Extension(
+    "tablecraft",
+    sources=[
+        "src/py_tablecraft_module.c",
+        "src/table.c",
+        "src/commands.c",
+        "src/errors.c",
+        "src/python_bridge.c",
+        "src/ui_draw.c",
+        "src/ui_edit.c",
+        "src/ui_init.c",
+        "src/ui_loop.c",
+        "src/utils.c",
+    ],
+    include_dirs=["include"],
+    libraries=["ncursesw"],
+)
+
+setup(
+    name="tablecraft",
+    version="0.1.0",
+    description="Terminal-based table editor (ncurses UI)",
+    ext_modules=[module],
+)
